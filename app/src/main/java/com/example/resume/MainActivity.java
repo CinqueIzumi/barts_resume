ad2f9fc9992a91d442864268076dcf897f349287
Bart <ba.peereboom@gmail.com>
2020-02-10 19:52:06 +0100

    Added the skeleton for the navigation bar
    (+): Fragments for all the navigation bar options
    (+): Navigation bar with icons
    (+): Logic to support switching between the fragments by using the navigation bar
    
    (~): Added several strings to the strings.xml file

--- app/src/main/java/com/example/resume/MainActivity.java
+++ app/src/main/java/com/example/resume/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.resume;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView botnav = findViewById(R.id.bottom_navigation);
+        botnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch(item.getItemId()) {
+                    case R.id.nav_about:
+                        selectedFragment = new AboutFragment();
+                        break;
+                    case R.id.nav_education:
+                        selectedFragment = new EducationFragment();
+                        break;
+                    case R.id.nav_projects:
+                        selectedFragment = new ProjectsFragment();
+                        break;
+                    case R.id.nav_skills:
+                        selectedFragment = new SkillsFragment();
+                        break;
+                    case R.id.nav_work:
+                        selectedFragment = new WorkFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
     }
 }
